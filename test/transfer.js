433ab7f630285c0540bf98b25eeddf0ff3f2eac5
liukan <liukan@bytedance.com>
2019-07-07 11:42:45 +0800

    update
    Change-Id: Ia2b88fca0fae4c339ae962088481fb6348a86275

--- test/transfer.js
+++ test/transfer.js
@@ -13,4 +13,29 @@ const loadSystemCell = async () => {
     return systemCellInfo
 }
 
-loadSystemCell().then(console.log)
+loadSystemCell()
+    .then(cell => {
+        function _hash(a) {
+            return core.rpc.paramsFormatter.toHash(a)
+        }
+
+        const ENCRYPT_CODE_HASH = _hash(cell.codeHash)
+
+        const ENCRYPT_CELL = {
+            blockHash: _hash(cell.outPoint.blockHash),
+            cell: {
+                txHash: _hash(cell.outPoint.cell.txHash),
+                index: cell.outPoint.cell.index,
+            }
+        }
+
+        const MyAddr = code.generateAddress(privateKey)
+
+        console.log(MyAddr)
+
+        const script = {
+            codeHash: ENCRYPT_CODE_HASH,
+            args: [`0x${MyAddr.idenfitier}`]
+        }
+    })
+
