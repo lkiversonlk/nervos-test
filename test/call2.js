69f27836b9b9bb78c07de61943c5539314d6ef9e
liukan <liukan@bytedance.com>
2019-07-08 23:35:40 +0800

    update
    Change-Id: If763cf1b7784a8b1d0097aed978fab46478cbf35

--- test/call2.js
+++ test/call2.js
@@ -1,6 +1,7 @@
 const core = require('../lib/init').init()
 const loadSys = require('../lib/systemcell')
 const loadTx = require('../lib/loadTxOutput').load
+const privateKey = require('../config.json').privateKey
 
 const txHash = "0x395fc85dc84950ee394447418405e33413e3c0700e12d6e67c98452e797b0a3f"
 const MyAddr = core.generateAddress(privateKey)
