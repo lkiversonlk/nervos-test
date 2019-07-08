58eed21109137c1d9e961397b4df2215aa66ea62
liukan <liukan@bytedance.com>
2019-07-08 23:25:36 +0800

    update
    Change-Id: Ib9589afb5c9b8dda2df12c56382666c5be396307

--- test/call2.js
+++ test/call2.js
@@ -1,6 +1,6 @@
 const core = require('../lib/init').init()
 const loadSys = require('../lib/systemcell')
-const loadTx = require('../lib/loadTxOutput')
+const loadTx = require('../lib/loadTxOutput').load
 
 const txHash = "0x395fc85dc84950ee394447418405e33413e3c0700e12d6e67c98452e797b0a3f"
 
