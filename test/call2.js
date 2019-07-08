2cb513f50d1b463359e5fb17cb093ec10da579ec
liukan <liukan@bytedance.com>
2019-07-08 23:24:21 +0800

    update
    Change-Id: I4ff17fcbbf0e49db765ba7aaec126571466f385e

--- test/call2.js
+++ test/call2.js
@@ -1,9 +1,16 @@
 const core = require('../lib/init').init()
 const loadSys = require('../lib/systemcell')
+const loadTx = require('../lib/loadTxOutput')
+
+const txHash = "0x395fc85dc84950ee394447418405e33413e3c0700e12d6e67c98452e797b0a3f"
 
 loadSys.loadSystemInfo(core)
     .then(SYS => {
         const { CODE_HASH, SYSTEM_CELL } = SYS
 
-        
+        loadTx(core, txHash)
+            .then(txData => {
+                // get tx output
+                console.log(JSON.stringify(txData, null, 2))
+            })
     })
