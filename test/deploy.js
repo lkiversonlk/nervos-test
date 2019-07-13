780a15a4a5773c8f1f02463bbfd46ce435d44bbc
liukan <liukan@bytedance.com>
2019-07-13 16:55:07 +0800

    update
    Change-Id: I46a21b2262952d17107d8dcee753c28d4b8488b5

--- test/deploy.js
+++ test/deploy.js
@@ -6,22 +6,24 @@ const loadCKBs = require('../lib/loadCKBs')
 const MyAddr = core.generateAddress(privateKey)
 
 const identifier = `0x${MyAddr.idenfitier}`
+
 function deployData(data) {
+    if (data.length === 0) {
+        console.log(`data length is 0`)
+        return
+    }
+
     return loadSys.loadSystemInfo(core)
         .then(SYS => {
             const { CODE_HASH, SYSTEM_CELL } = SYS
-
             return loadCKBs.loadCKBs(CODE_HASH, identifier, core)
                 .then(cells => {
-                    console.log(cells)
-
                     let capacity = 0
                     const inputs = []
                     const witnesses = []
 
                     cells.forEach(cell => {
                         capacity += parseInt(cell.capacity)
-                        console.log(cell)
                         inputs.push({
                             previousOutput: cell.outPoint,
                             since: '0'
@@ -30,9 +32,7 @@ function deployData(data) {
                             data:[]
                         })
                     })
-
                     console.log(`total capacity is ${capacity}`)
-
                     const changed = {
                         capacity: 0,
                         lock: {
@@ -42,34 +42,35 @@ function deployData(data) {
                         data: '0x'
                     }
 
-                    let capacityNeeded = 6000000000
+                    let capacityNeeded = 0// 6000000000
+                    const outputs = []
 
-                    if (data.length > 2) {
-                        capacityNeeded += (100000000 * (data.length - 2)/2)
+                    for (let i = 0; i < data.length; i ++) {
+                        const d = data[i]
+                        let cellCapacity = 6000000000
+                        if (d.length > 2) {
+                            cellCapacity += (100000000 * (data.length - 2)/2)
+                        }
+                        outputs.push({
+                            capacity: cellCapacity.toString(),
+                            lock: {
+                                codeHash: CODE_HASH,
+                                args: [identifier]
+                            },
+                            data: d
+                        })
+                        capacityNeeded += cellCapacity
                     }
 
                     if (capacity < capacityNeeded) {
                         return Promise.reject(`need capacity ${capacityNeeded}, but only have ${capacity}`)
                     }
 
-                    const codeOutput = {
-                        capacity: capacityNeeded.toString(),
-                        lock: {
-                            codeHash: CODE_HASH,
-                            args: [identifier]
-                        },
-                        data
-                    }
-
                     const left = capacity - capacityNeeded
-                    const outputs = [codeOutput]
-
                     if (left > 0) {
                         changed.capacity = left.toString()
                         outputs.push(changed)
                     }
-
-
 
                     const tx = {
                         version: '0',
@@ -91,9 +92,10 @@ function deployData(data) {
 
 // const tryCapacity = parseInt(process.argv[2])
 const fs = require('fs')
-const file = process.argv[2]
-
-const data = '0x' + fs.readFileSync(file).toString('hex')
+const data = []
+for (let i = 0; i < process.argv.length - 2; i ++) {
+    data[i] = '0x' + fs.readFileSync(process.argv[i + 2]).toString('hex')
+}
 
 deployData(data)
     .then(txhash => {
