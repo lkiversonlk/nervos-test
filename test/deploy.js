48bbbe709e18aef85fba08aeefd1e1db39a5c192
liukan <liukan@bytedance.com>
2019-07-13 15:56:30 +0800

    update
    Change-Id: I1a25a6f273e821226819e48b453f865f7b69ce5e

--- test/deploy.js
+++ test/deploy.js
@@ -91,7 +91,7 @@ function deployData(data) {
 const fs = require('fs')
 const file = process.argv[2]
 
-const data = fs.readFileSync(file).toString('hex')
+const data = '0x' + fs.readFileSync(file).toString('hex')
 
 deployData(data)
     .then(txhash => {
